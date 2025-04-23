11e066266d61e10c431dfbe7fc529dd63b8ff2c4
Thomas Lunnen III <60558126+wLunnen@users.noreply.github.com>
2025-04-23 11:54:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.querySelectorAll('.flip-box').forEach(card => {
   });
 });
 function openMultiplePages() {
-       window.open("https://www.linkedin.com/company/western-development-initiative/", "_blank");
-       window.open("https://wlunnen.github.io/Co/", "_blank");
-   }
+   window.open("https://www.linkedin.com/company/western-development-initiative/", "_blank");
+   window.open("https://wlunnen.github.io/Co/", "_blank");
+};
