b6009daaf3b7e3d92553dfd8f8250d261754b22b
Thomas Lunnen III <60558126+wLunnen@users.noreply.github.com>
2025-05-05 10:15:59 -0700

    Create script.js

---
+++ legacy/script.js
@@ -0,0 +1,18 @@
+/*
+//card logic
+document.addEventListener("DOMContentLoaded", () => {
+  // Placeholder: Interactive card logic to be implemented
+});
+document.getElementByClassName('card') {
+  onClick="function(){
+    ;
+    return false;
+};
+*/
+document.getElementById('click') {
+  onClick="function(){
+    position: absolute;
+    z-index: 1;
+    return false;
+};
+};
