24384a58b3126f97bb2a4aa7a2288e2603cd849f
Thomas Lunnen III <60558126+wLunnen@users.noreply.github.com>
2025-06-09 17:12:05 -0700

    Update random.js

--- scripts/random.js
+++ scripts/random.js
@@ -1,9 +1,16 @@
-let 1 = 
-let 2 = 
-let 3 = 
+let 1 = "https://i.postimg.cc/8CCP8R48/bw-bg.png";
+let 2 = "https://i.postimg.cc/vmMNH02p/teal-bg.png";
 
-let array1 = [1, 2, 3];
+let 3 = "https://i.postimg.cc/B6QBRhhX/blue.jpg";
+let 4 = "https://i.postimg.cc/B6QBRhhX/red.jpg";
+let 5 = "https://i.postimg.cc/J4tTs067/OG-BR.png";
+
+let 6 = "https://i.postimg.cc/B6QBRhhX/blue.jpg";
+
+let array1 = [1, 2];
+let array2 = [3, 4, 5];
+let array3 = [array1, array2];
 
 background (bg) {
   random
-}
+};
